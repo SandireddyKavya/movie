6247b0fcf6dc08533a66c0280fb737fa1e54a962
Naga Anvesh <knanvesh.01@gmail.com>
2021-05-16 20:27:40 +0530

    Adding save functionality for Image Manipulation

--- src/main/java/com/anvesh/recepieapp/services/ImageServiceImp.java
+++ src/main/java/com/anvesh/recepieapp/services/ImageServiceImp.java
@@ -1,19 +1,47 @@
 package com.anvesh.recepieapp.services;
 
+import com.anvesh.recepieapp.domain.Recipe;
+import com.anvesh.recepieapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.util.Optional;
 
 @Service
 @Slf4j
 public class ImageServiceImp implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImp(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
         log.debug("Image is being Uploaded");
+        try {
+            Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+            Byte[] bytesObject = new Byte[file.getBytes().length];
+            int k = 0;
+            for (byte i : file.getBytes()) {
+                bytesObject[k++] = i;
+            }
+            if (recipeOptional.isPresent()) {
+                Recipe recipe = recipeOptional.get();
+                recipe.setImage(bytesObject);
+                recipeRepository.save(recipe);
+            } else {
+                throw new RuntimeException("Recipe Not found : " + recipeId);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         log.debug("Image Uploaded");
     }
 }
--- src/test/java/com/anvesh/recepieapp/services/ImageServiceImpTest.java
+++ src/test/java/com/anvesh/recepieapp/services/ImageServiceImpTest.java
@@ -1,15 +1,44 @@
 package com.anvesh.recepieapp.services;
 
+import com.anvesh.recepieapp.domain.Recipe;
+import com.anvesh.recepieapp.repositories.RecipeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class ImageServiceImpTest {
+
+    @InjectMocks
+    ImageServiceImp imageServiceImp;
+
+    @Mock
+    RecipeRepository recipeRepository;
 
     @BeforeEach
     void setUp() {
     }
 
     @Test
-    void saveImageFile() {
+    void saveImageFile() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        MultipartFile file = new MockMultipartFile("imagefile", "testne.txt", "text/plain", "hello this anvesh".getBytes());
+        when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(recipe));
+        imageServiceImp.saveImageFile(1L, file);
+        verify(recipeRepository, times(1)).save(same(recipe));
+        assertEquals(file.getBytes().length, recipe.getImage().length);
+
     }
 }
